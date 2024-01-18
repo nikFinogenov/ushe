574192f7a687b6df95bfa0bf66732249ceb2885d
adm <xdxddd932@gmail.com>
2024-01-18 11:45:48 -0800

    zaglushka+

--- src/main.c
+++ src/main.c
@@ -18,6 +18,8 @@ int main(void) {
             mx_strcpy(commands[i], mx_strtrim(command_format(commands[i])));
             char* command = mx_strsplit(commands[i], ' ')[0];
             if (strcmp(command, "pwd") == 0) pwd();
+            else if (strcmp(command, "export") == 0) return 0;
+            else if (strcmp(command, "unset") == 0) return 0;
             else if (strcmp(command, "cd") == 0) cd(mx_strsplit(commands[i], ' ')[1]);
             else if (strcmp(command, "echo") == 0) echo(commands[i]);
             else if (strcmp(command, "exit") == 0) my_exit();//somehow need to manage status
